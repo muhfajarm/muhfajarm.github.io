0d9927d8d02608b3e089e51a5eaa8002d74a05fd
MqD <muh.fajar12642@gmail.com>
2022-04-26 21:59:14 +0700

    Menambahkan script getJSON dan memindahkan script lazyload

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+$(window).on("load", function() {
+	$.getJSON("assets/data/data.json", function(data){
+		// console.log(data.portfolio)
+
+		// Append Intro
+		$('.intro').append(data.name)
+		$('.intro-job').append(data.job)
+		$('.intro-social').append('<a class="text-decoration-none" href="'+data.social.twitter+'" target="_blank"><span class="mx-1"><i class="fab fa-twitter" style="font-size: 24px; color: #0284B3;"></i></span></a><a class="text-decoration-none" href="'+data.social.instagram+'" target="_blank"><span class="mx-1"><i class="fab fa-instagram" style="font-size: 24px; color: #0284B3;"></i></span></a><a class="text-decoration-none" href="'+data.social.github+'" target="_blank"><span class="mx-1"><i class="fab fa-github" style="font-size: 24px; color: #0284B3;"></i></span></a><br><a href="#contact" class="btn btn-primary mt-2" data-aos="fade-right" data-aos-duration="1000">Contact</a>')
+		$('.intro-photo').append('<img class="img-fluid" src="./assets/img/'+data.about.img+'" alt="Muhammad Fajar Maqidi" data-aos="fade-left" data-aos-duration="1000">')
+		// End Append Intro
+		
+		// Append About
+		$('.about-me').append(data.about.me)
+		$('.about-me-sm').append(data.about.me)
+		// End About
+		// 
+		// Append Skills
+		$('.skill-uiux').append(data.skills.uiux)
+		$('.skill-programming').append(data.skills.programming.join(', '))
+		// End Skills
+
+		// Append Portfolio
+		$.each( data.portfolio, function( key, val ) {
+			console.log(val)
+			$('.portfolio').append('<figure><img src="./assets/img/'+val.img.thumb+'" alt="Portfolio BPMI-SDG" class="rounded img-fluid" data-src="./assets/img/'+val.img.ori+'"><figcaption><a class="title fw-bold text-decoration-none" href="'+val.url+'" target="_blank">'+val.name+'</a><small class="py-1 d-md-none">'+val.description+'</small></figcaption><div class="overlay"></div></figure>')
+		})
+
+		// Lazyload Script
+		function callback(entries) {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					const imgElement = entry.target.querySelector('img')
+					const realSrc = imgElement.dataset.src;
+					imgElement.style.filter = 'blur(0px)'
+					imgElement.setAttribute('src', realSrc)
+				}
+			})
+		}
+
+		const options = {
+			threshold: 0.1
+		}
+
+		let observer = new IntersectionObserver(callback, options)
+		observer.observe(document.querySelector('.portfolio'))
+		// End Append Portfolio
+		
+		// Append Footer
+		$('.footer-social').append('<a class="text-decoration-none" href="'+data.social.twitter+'" target="_blank"><span class="mx-1"><i class="fab fa-twitter" style="font-size: 24px; color: #0284B3;"></i></span></a><a class="text-decoration-none" href="'+data.social.instagram+'" target="_blank"><span class="mx-1"><i class="fab fa-instagram" style="font-size: 24px; color: #0284B3;"></i></span></a><a class="text-decoration-none" href="'+data.social.github+'" target="_blank"><span class="mx-1"><i class="fab fa-github" style="font-size: 24px; color: #0284B3;"></i></span></a>')
+		// End Append Footer
+	})
+})
